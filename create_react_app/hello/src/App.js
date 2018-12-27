d5f66e243058ddcc483aee1f96991b8750e369d4
dla1434 <dla1434@naver.com>
2018-12-27 19:04:29 +0900

    webpack ing

---
+++ create_react_app/hello/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h2>Hellow World!</h2>
+    );
+  }
+}
+
+export default App;
