f5ea10a17ac82fb344ec4c1e4c6910994d826337
dla1434 <dla1434@naver.com>
2019-01-03 12:57:20 +0900

    react 20강 - lifeCycle 완료

---
+++ create_react_app/stopwatch/src/StopWatch_0001.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StopWatch extends Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default StopWatch;
---
+++ create_react_app/stopwatch/src/StopWatch_0003.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class StopWatch extends Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			isLive: false,
+			curTime: 0,
+			startTime: 0
+		}
+		this.timerId = 0
+	}
+
+	componentWillMount() {
+		this.timerId = setInterval(e => { this.tick() }, 1000)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerId)	
+	}
+
+  render() {
+		let label = 'Start'
+		if(this.state.isLive){
+			label = 'Stop'
+		}
+    return (
+      <div>
+				<div>{this.state.curTime - this.state.startTime}</div>
+				<button onClick={(e) => {
+					if (this.state.isLive) {
+						//동작되고 있다면 STOP
+						this.setState({
+							isLive: false
+						})
+					}
+					else{
+						//미동작되고 있다면 START
+						const currentTime = new Date().getTime()
+						this.setState({
+							isLive: true,
+							curTime: currentTime,
+							startTime: currentTime
+						})
+					}
+				}}>{label}</button>
+			</div>
+    );
+  }
+}
+
+export default StopWatch;
